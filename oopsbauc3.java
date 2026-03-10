249c7c3d120971afb0cb69a668b856504703f709
Felix-x07 <fb1333@srmist.edu.in>
2026-03-10 14:15:23 +0530

    uc3 added

---
+++ oopsbauc3.java
@@ -0,0 +1,13 @@
+public class oopsbauc3 {
+ public static void main(String args[]){
+System.out.println(String.join(" ","   ***   ","   ***   ","******   ","   *****  "));
+System.out.println(String.join(" "," **   ** "," **   ** ","*     ** "," **       "));
+System.out.println(String.join(" ","**     **","**     **","*      **","**        "));
+System.out.println(String.join(" ","**     **","**     **","*     ** "," **       "));
+System.out.println(String.join(" ","**     **","**     **","******   ","   *****  "));
+System.out.println(String.join(" ","**     **","**     **","*        ","       ** "));
+System.out.println(String.join(" ","**     **","**     **","*        ","        **"));
+System.out.println(String.join(" "," **   ** "," **   ** ","*        ","       ** "));
+System.out.println(String.join(" ","   ***   ","   ***   ","*        ","   *****  "));
+    }
+}
